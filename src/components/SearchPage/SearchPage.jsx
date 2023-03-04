b2445e6af1c4c9f0e2767b1e07c00d1c065a42b6
Akin GURLER <akingurler98@hotmail.com>
2023-03-04 20:17:29 +0300

    page for Search operation

---
+++ src/components/SearchPage/SearchPage.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import { getSearchedProducts } from '../../actions/products';
+import Products from '../Products/Products';
+import "./SearchPage.scss"
+
+const SearchPage = () => {
+    const { searchTerm } = useParams();
+    const products = useSelector((state) => state.homePageProducts)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getSearchedProducts(searchTerm))
+    }, [searchTerm])
+
+    return (
+        
+        <div className='search-page-container'>
+            <h2>Result For <span>{searchTerm}</span></h2>
+            <Products products={products} />
+        </div>
+    )
+}
+
+export default SearchPage
